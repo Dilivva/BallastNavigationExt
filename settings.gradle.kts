2de49d9a4bd425fb4f3d305a3219e16520de82a1
Ayodele Kehinde <ayodelekehinde8@send24.co>
2024-03-18 15:46:34 +0100

    Start type safe navigation using KSP

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
     }
 }
 
